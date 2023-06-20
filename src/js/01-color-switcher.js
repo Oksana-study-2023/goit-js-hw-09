9cc543ea341b85b6a586f14316dfd4e0adf68ccc
Oksana <oks46@ukr.net>
2023-06-20 18:12:00 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,29 @@
-const bodyEl = document.querySelector(`body`);
-const btnStart = document.querySelector(`button[data-start]`);
-const btnStop = document.querySelector(`button[data-stop]`);
-const COLOR_CHANGE_TIME = 1000;
+// const bodyEl = document.querySelector(`body`);
+let btnStart = document.querySelector(`button[data-start]`);
+console.log(btnStart);
+// const btnStop = document.querySelector(`button[data-stop]`);
+// const COLOR_CHANGE_TIME = 1000;
+
+// btnStart.addEventListener(`click`, onBtnStartClick);
+
+// btnStop.addEventListener(`click`, onBtnStopClick);
 
 
-btnStart.addEventListener(`click`, onBtnStartClick);
-btnStop.addEventListener(`click`, onBtnStopClick);
+//  function onBtnStartClick() {
+//     console.log(`start`);
+//  setTimeout(() => {
+//      getRandomHexColor();
+//     }, COLOR_CHANGE_TIME);
+//   };
 
-function onBtnStartClick() {
-    console.log(`start`);
-    // setTimeout(() => {
-    //     getRandomHexColor();
-    // }, COLOR_CHANGE_TIME);
-};
+// function onBtnStopClick() {
+// console.log(`stop`);
+//  };
 
-function onBtnStopClick() {
-    console.log(`stop`);
-};
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-// }
+//  function getRandomHexColor() {
+//    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+//  }
+const date = new Date();
+console.log(date);
 
 
